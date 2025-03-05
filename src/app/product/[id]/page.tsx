988bffb8047b90f6efc800b0ff0ee4594883b0ff
Mithu225 <thu@opendigital.no>
2025-03-05 15:30:43 +0100

    fix error use client

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import ProductDetails from "@/components/ProductDetails";
 import { useParams } from "next/navigation";
 
 export default function ProductPage() {
   const params = useParams<{ id: string }>();
-  
+
   if (!params?.id) return null;
   return <ProductDetails productId={params.id} />;
 }
